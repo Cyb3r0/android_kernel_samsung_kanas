4b1ccecc2caf3ab3a63ac07d06c7cc3c9e13a63e
ih24n69 <muhammadihsan69@gmail.com>
2019-04-17 22:16:55 +0700

    cpufreq: added smartass2 governor
    ref:https://github.com/diepquynh1501/android_kernel_samsung_grandprimeve3g/commit/eeb794666feb2435c835eea2b55e39f347937218

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -416,6 +416,8 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
 #endif
 
 /*********************************************************************
