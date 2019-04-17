71c5c76a98f1f80a4c5591797dcf647d2ca8a4ba
ih24n69 <muhammadihsan69@gmail.com>
2019-04-17 15:43:57 +0700

    cpufreq: intelliactive: added new faux123 governor
    ref:https://github.com/diepquynh1501/android_kernel_samsung_grandprimeve3g/commit/4ea7a97fac7ea76a3d1c99662fc472c810fae1c2

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -412,6 +412,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #endif
 
 /*********************************************************************
